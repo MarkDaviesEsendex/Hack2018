2536f373c90d0b4a0802460f4b75db365983cb1f
Mark Davies <mark.davies@esendex.com>
2018-03-10 14:52:10 +0000

    Starting getting information passed into services

---
+++ src/src/Esendexers.HomelessWays.Application/Inputs/CreateIncidentInput.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+using Abp.AutoMapper;
+using Esendexers.HomelessWays.Entities;
+
+namespace Esendexers.HomelessWays.Inputs
+{
+    [AutoMapTo(typeof(Incident))]
+    public class CreateIncidentInput
+    {
+        public string Latitude { get; set; }
+        public string Longitude { get; set; }
+        public DateTime Time { get; set; }
+        public string Description { get; set; }
+        public Stream ImageStream { get; set; }
+    }
+}
---
+++ src/src/Esendexers.HomelessWays.Web/Controllers/TestController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Esendexers.HomelessWays.Inputs;
+using Esendexers.HomelessWays.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Esendexers.HomelessWays.Web.Controllers
+{
+    public class TestController : HomelessWaysControllerBase
+    {
+        private readonly ILanguageAnalysysService _languageAnalysys;
+        private readonly IIncidentAppService _incidentAppService;
+
+        public TestController(ILanguageAnalysysService languageAnalysys, IIncidentAppService incidentAppService)
+        {
+            _languageAnalysys = languageAnalysys;
+            _incidentAppService = incidentAppService;
+        }
+
+        public IActionResult Test(string description) 
+            => Ok(_languageAnalysys.GetSentimentScore(description));
+
+        public IActionResult Incident()
+        {
+            _incidentAppService.RecordNewIncident(new CreateIncidentInput
+            {
+                Description = "REally bad graffiti",
+                Latitude = "Hello",
+                Longitude = "Hello",
+                Time = DateTime.Now
+            });
+            return Ok();
+        }
+    }
+}
