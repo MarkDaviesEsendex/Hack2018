052c197dff5416f0c2e807f2d255131c401e421d
ESENDEX\louis.wright <louis.wright@esendex.com>
2018-03-10 12:54:49 +0000

    Added incidents controller, and coords model.

---
+++ src/src/Esendexers.HomelessWays.Web/Controllers/IncidentsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis;
+
+namespace Esendexers.HomelessWays.Web.Controllers
+{
+    public class IncidentsController : HomelessWaysControllerBase
+    {
+        [HttpGet]
+        public IActionResult IncidentsNearby(Coordinates currentLocation, uint radius)
+        {
+//            currentLocation.
+            return Json("hi");
+        }
+    }
+
+    public class Coordinates
+    {
+        public string Latitude { get; set; }
+        public string Longitude { get; set; }
+    }
+}
