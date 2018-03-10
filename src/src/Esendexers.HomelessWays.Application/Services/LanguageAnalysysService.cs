3c49fc51032404caaa5710aa2779ecf098410f93
Mark Davies <mark.davies@esendex.com>
2018-03-10 14:19:34 +0000

    ADding sentiment analysis

---
+++ src/src/Esendexers.HomelessWays.Application/Services/LanguageAnalysysService.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Azure.CognitiveServices.Language.TextAnalytics;
+using Microsoft.Azure.CognitiveServices.Language.TextAnalytics.Models;
+
+namespace Esendexers.HomelessWays.Services
+{
+    public class LanguageAnalysysService :  HomelessWaysAppServiceBase, ILanguageAnalysysService
+    {
+        public double GetSentimentScore(string description)
+        {
+            var client = new TextAnalyticsAPI
+            {
+                AzureRegion = AzureRegions.Westcentralus,
+                SubscriptionKey = "48bb9c86473d4de58d5df8fe98c2b5f4"
+            };
+
+            var result = client.DetectLanguage(new BatchInput(new List<Input> {new Input("1", description)}));
+            var language = result.Documents.First().DetectedLanguages.First();
+
+            var sentiment = client.Sentiment(new MultiLanguageBatchInput(
+                new List<MultiLanguageInput> {new MultiLanguageInput(language.Iso6391Name, "0", description)}));
+            return sentiment.Documents.First().Score ?? 0;
+        }
+    }
+
+    public interface ILanguageAnalysysService
+    {
+        double GetSentimentScore(string description);
+    }
+}
---
+++ src/src/Esendexers.HomelessWays.Web/Controllers/LanguageTestController.cs
@@ -0,0 +1,18 @@
+﻿using Esendexers.HomelessWays.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Esendexers.HomelessWays.Web.Controllers
+{
+    public class LanguageTestController : HomelessWaysControllerBase
+    {
+        private readonly ILanguageAnalysysService _languageAnalysys;
+
+        public LanguageTestController(ILanguageAnalysysService languageAnalysys)
+        {
+            _languageAnalysys = languageAnalysys;
+        }
+
+        public IActionResult Test(string description) 
+            => Ok(_languageAnalysys.GetSentimentScore(description));
+    }
+}
