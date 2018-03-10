5b5b129dec545b93cc14b377e4d1b2fe26efae29
Mark Davies <mark.davies@esendex.com>
2018-03-10 12:51:10 +0000

    Adding submit endpoint

---
+++ src/src/Esendexers.HomelessWays.Web/Controllers/SubmitController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Esendexers.HomelessWays.Web.Controllers
+{
+    public class SubmitController : HomelessWaysControllerBase
+    {
+        [HttpPost]
+        public IActionResult RecordIncident(IncidentModel incident)
+        {
+            return Ok(true);
+        }
+    }
+
+    public class IncidentModel
+    {
+        public IFormFile IncidentImage { get; set; }
+        public string Latitude { get;set; }
+        public string Longitude { get; set; }
+        public string Description { get; set; }
+    }
+}
